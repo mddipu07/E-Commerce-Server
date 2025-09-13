30397d589c7ddf9ec551ddc84e88ab465733e5ab
Md <dipum0505@gmail.com>
2025-09-13 17:24:35 +0600

    create config file , contorller,middleware,models,routes and all functionality done

---
+++ controllers/productController.js
@@ -0,0 +1,22 @@
+// Add Product : /api/product/add
+
+export const addProduct = async(req,res) => {
+     
+}
+
+
+// Get Product : /api/product/list
+export const productList = async(req,res) => {
+    
+}
+
+// Get single Product : /api/product/id
+export const productById = async(req,res) => {
+
+}
+
+
+// change product inStock : /api/product/stock
+export const ChangeStock = async(req,res) => {
+
+}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors'
-const app = express();
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
 
+
+const app = express();
 const port = process.env.PORT || 4000;
 
 // Allow multiple origins
@@ -11,11 +17,16 @@ const allowdOrigins = ['http://localhost:5173']
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowdOrigins, Credentials: true}));
+app.use(cors({origin: allowdOrigins, credentials: true}));
 
+await connectDB()
+await connectCloudinary()
 
 app.get('/', (req,res) => 
     res.send("API is Working"));
+
+app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
 
 
 app.listen(port,()=>{
